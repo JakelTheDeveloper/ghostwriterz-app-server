d376421f58fa598ff26f67180f52a0cc4c6098f0
JaKel Jenkins <jakelj24@gmail.com>
2020-11-10 13:49:53 -0500

    all endpoints functional

--- src/app.js
+++ src/app.js
@@ -4,14 +4,18 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const winston = require('winston');
+const { v4: uuid } = require('uuid');
 const { NODE_ENV } = require('./config')
 
 const app = express()
 
-const lyrics = [
+app.use(express.json());
+
+const lyricData = [
   {
       id:1,
       title: "Hello",
+      rating: 5,
       genre:"Rap",
       mood:"Happy",
       artist:"Foo",
@@ -21,6 +25,7 @@ const lyrics = [
   {
       id:2,
       title: "Bye",
+      rating: 3,
       genre:"Soul",
       mood:"Sad",
       artist:"Bar",
@@ -30,6 +35,7 @@ const lyrics = [
   {
       id:3,
       title: "See You Again",
+      rating: 2,
       genre:"Rock",
       mood:"Energetic",
       artist:"Lee",
@@ -72,12 +78,12 @@ app.get('/', (req, res) => {
 
      app.get('/lyrics', (req, res) => {
       res
-        .json(lyrics);
+        .json(lyricData);
     });
 
     app.get('/lyrics/:id', (req, res) => {
       const { id } = req.params;
-      const lyric = lyrics.find(lyric => lyric.id == id);
+      const lyric = lyricData.find(lyric => lyric.id == id);
     
       // make sure we found a card
       if (!lyric) {
@@ -88,6 +94,86 @@ app.get('/', (req, res) => {
       }
     
       res.json(lyric);
+    });
+
+    app.post('/lyrics', (req, res) => {
+      const { title, rating, genre, mood, artist, lyrics } = req.body;
+      if (!title) {
+        logger.error(`title is required`);
+        return res
+          .status(400)
+          .send('Invalid data');
+      }
+      if (!Number.isInteger(rating) || rating < 0 || rating > 5) {
+        logger.error(`Invalid rating '${rating}' supplied`)
+        return res.status(400).send(`'rating' must be a number between 0 and 5`)
+      }
+      if (!genre) {
+        logger.error(`Please select a genre for your lyrics`);
+        return res
+          .status(400)
+          .send('Please select a genre for your lyrics');
+      }
+      if (!mood) {
+        logger.error(`Please select a mood for your lyrics`);
+        return res
+          .status(400)
+          .send('Please select a mood for your lyrics');
+      }
+      if (!artist) {
+        logger.error(`Please provide an artist name for your lyrics`);
+        return res
+          .status(400)
+          .send('Please provide an artist name for your lyrics');
+      }
+      if (!lyrics) {
+        logger.error(`Please provide Lyrics`);
+        return res
+          .status(400)
+          .send('Please provide Lyrics');
+      }
+      // get an id
+      const id = uuid();
+    
+      const newLyrics = {
+        id,
+        title,
+        rating,
+        genre,
+        mood,
+        artist,
+        lyrics,
+        expanded:false
+      };
+    
+      lyricData.push(newLyrics);
+    
+      logger.info(`Lyrics with id ${id} created`);
+    
+      res
+        .status(201)
+        .location(`http://localhost:8000/lyrics/${id}`)
+        .json({id});
+    });
+
+    app.delete('/lyrics/:id', (req, res) => {
+      const { id } = req.params;
+    
+      const lyricIndex = lyricData.findIndex(li => li.id == id);
+    
+      if (lyricIndex === -1) {
+        logger.error(`Lyrics with id ${id} not found.`);
+        return res
+          .status(404)
+          .send('Not Found');
+      }
+    
+      lyricData.splice(lyricIndex, 1);
+    
+      logger.info(`Lyrics with id ${id} deleted.`);
+      res
+        .status(204)
+        .end();
     });
 
      app.use(function errorHandler(error, req, res, next) {
