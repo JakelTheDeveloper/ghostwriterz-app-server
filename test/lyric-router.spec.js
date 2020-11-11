c941faf42e4c70dc6fc721bea1de0bca7e154bb3
JaKel Jenkins <jakelj24@gmail.com>
2020-11-11 01:09:03 -0500

    updated tests

--- test/lyric-router.spec.js
+++ test/lyric-router.spec.js
@@ -1,36 +1,3 @@
-// const { expect } = require('chai');
-
-// const lyric_router = require('../src/Lyrics/lyric-router');
-// const supertest = require('supertest');
-
-// describe('GET /lyrics',()=>{
-// it('Should show lyrics database should be an array',()=>{
-//   const query = [
-//     {
-//         id:1,
-//         title: "Hello",
-//         rating: 5,
-//         genre:"Rap",
-//         mood:"Happy",
-//         artist:"Foo",
-//         lyrics:"Leo sociosqu sagittis nascetur netus congue? Dapibus cubilia praesent nam magnis ante felis Leo sociosqu sagittis nascetur netus congue? Dapibus cubilia praesent nam magnis ante felis Leo sociosqu sagittis nascetur netus congue? Dapibus cubilia praesent nam magnis ante felis",
-//         expanded:false
-//     }
-//   ]
-//     return supertest(lyric_router)
-//     .get('/lyrics')
-//     .query(query)
-//     .expect(200) // supertest expect
-//     .expect('Content-Type', /json/)
-//     .then(res => {
-//       expect(res.body).to.have.all.keys('array');
-//       // expect(res.body).to.have.all.keys('title','rating','genre','mood','artist','lyrics');
-//       // array must not be empty
-//       expect(res.body).to.have.lengthOf.at.least(1);
-//     })
-// })
-// })
-
 const { expect } = require('chai')
 const supertest = require('supertest')
 const app = require('../src/app');
@@ -39,9 +6,9 @@ describe('Lyric-Router', () => {
   it('GET / responds with 200 containing Data', () => {
     return supertest(app)
       .get('/lyrics')
+      .expect(200)
       .expect('Content-Type', /json/)
       .then(res => {
-        // expect(200,res.body)
         expect(res.body).to.be.an('array');
         expect(res.body).to.have.lengthOf.at.least(1);
         expect(res.body[0]).to.have.all.keys('id','title','rating','genre','mood','artist','lyrics','expanded');
