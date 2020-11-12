38140b8748862e5a9c8cbdf0842dcd35b507a049
JaKel Jenkins <jakelj24@gmail.com>
2020-11-11 23:30:16 -0500

    lyric-server all tests added

--- src/lyric-service.js
+++ src/lyric-service.js
@@ -1,9 +1,37 @@
 const knex = require("knex")
 
 const LyricService = {
-    getAllArticles(knex){
+    getAllLyrics(knex) {
         return knex.select('*').from('lyric_data')
-    }
+    },
+    insertLyric(knex, newLyric) {
+        // return Promise.resolve({})
+        return knex
+            .insert(newLyric)
+            .into('lyric_data')
+            .returning('*')
+            //if you dont .then(rows) you'll get {object (id,title,genre,...)}
+            .then(rows => {
+                return rows[0]
+            })
+    },
+    getById(knex, id) {
+        return knex
+            .from('lyric_data')
+            .select('*')
+            .where('id', id)
+            .first()
+    },
+    deleteLyrics(knex, id) {
+        return knex('lyric_data')
+            .where({ id })
+            .delete()
+    },
+    updateLyrics(knex, id, newLyricFields) {
+        return knex('lyric_data')
+            .where({ id })
+            .update(newLyricFields)
+    },
 }
 
 module.exports = LyricService
--- test/lyric-service.spec.js
+++ test/lyric-service.spec.js
@@ -12,7 +12,7 @@ describe(`Articles service object`, function () {
     let db
     let testData = [
         {
-            id: 20,
+            id: 1,
             title: "Hello",
             genre: "Rap",
             mood: "Happy",
@@ -20,7 +20,7 @@ describe(`Articles service object`, function () {
             lyrics: "Leo sociosqu sagittis nascetur netus congue? Dapibus cubilia praesent nam magnis ante felis Leo sociosqu sagittis nascetur netus congue? Dapibus cubilia praesent nam magnis ante felis Leo sociosqu sagittis nascetur netus congue? Dapibus cubilia praesent nam magnis ante felis"
         },
         {
-            id: 21,
+            id: 2,
             title: "Bye",
             genre: "Soul",
             mood: "Sad",
@@ -28,7 +28,7 @@ describe(`Articles service object`, function () {
             lyrics: "Leo sociosqu sagittis nascetur netus congue? Dapibus cubilia praesent nam magnis ante felis Leo sociosqu sagittis nascetur netus congue? Dapibus cubilia praesent nam magnis ante felis Leo sociosqu sagittis nascetur netus congue? Dapibus cubilia praesent nam magnis ante felis"
         },
         {
-            id: 22,
+            id: 3,
             title: "See You Again",
             genre: "Rock",
             mood: "Energetic",
@@ -36,7 +36,7 @@ describe(`Articles service object`, function () {
             lyrics: "Leo sociosqu sagittis nascetur netus congue? Dapibus cubilia praesent nam magnis ante felis Leo sociosqu sagittis nascetur netus congue? Dapibus cubilia praesent nam magnis ante felis Leo sociosqu sagittis nascetur netus congue? Dapibus cubilia praesent nam magnis ante felis"
         },
     ]
-    
+
     before(() => {
         db = knex({
             client: 'pg',
@@ -45,23 +45,94 @@ describe(`Articles service object`, function () {
     })
 
     before(() => db('lyric_data').truncate())
-
-    before(() => {
-        return db
-            .into('lyric_data')
-            .insert(testData)
-    })
+    afterEach(() => db('lyric_data').truncate())
     after(() => db.destroy())
-    describe(`getAllArticles`, () => {
-        it(`resolves all articles from 'blogful_articles' table`, () => {
+
+    context(`Given 'lyric_data' has data`, () => {
+        beforeEach(() => {
+            return db
+                .into('lyric_data')
+                .insert(testData)
+        })
+
+        it(`getAllLyrics() resolves all lyrics from 'lyrics_data' table`, () => {
             // test that ArticlesService.getAllArticles gets data from table
-            return LyricService.getAllArticles(db)
+            return LyricService.getAllLyrics(db)
                 .then(actual => {
-                    expect(actual).to.eql(testData) 
-                    expect(actual[0].title).to.eql(actual.map(i=>{
-                        i[0].title
-                 }))
-            })
+                    expect(actual).to.eql(testData)
+                })
+        })
+
+        it(`getById() resolves lyric by id from 'lyric_data' table`, () => {
+            const thirdId = 3
+            const thirdTestLyric = testData[thirdId - 1]
+            return LyricService.getById(db, thirdId)
+                .then(actual => {
+                    expect(actual).to.eql({
+                        id: thirdId,
+                        title: thirdTestLyric.title,
+                        genre: thirdTestLyric.genre,
+                        mood: thirdTestLyric.mood,
+                        artist: thirdTestLyric.artist,
+                        lyrics: thirdTestLyric.lyrics
+                    })
+                })
+        })
+        it(`deleteLyric() removes lyric by id from 'lyric_data' table`, () => {
+            const lyricId = 3
+            return LyricService.deleteLyrics(db, lyricId)
+                .then(() => LyricService.getAllLyrics(db))
+                .then(allLyrics => {
+                    // copy the test lyrics array without the "deleted" lyric
+                    const expected = testData.filter(lyric => lyric.id !== lyricId)
+                    expect(allLyrics).to.eql(expected)
+                })
+        })
+        it(`updateLyrics() updates lyrics from the 'lyric_data' table`, () => {
+            const idOfLyricsToUpdate = 3
+            const newLyricData = {
+                title: 'updated title',
+                genre: 'Rap',
+                mood: 'Happy',
+                artist: 'updated artist',
+                lyrics: 'updated lyrics'
+            }
+            return LyricService.updateLyrics(db, idOfLyricsToUpdate, newLyricData)
+                .then(() => LyricService.getById(db, idOfLyricsToUpdate))
+                .then(lyric => {
+                    expect(lyric).to.eql({
+                        id: idOfLyricsToUpdate,
+                        ...newLyricData,
+                    })
+                })
+        })
+    })
+    context(`Given 'lyric_data' has no data`, () => {
+        it(`getAllLyrics() resolves an empty array`, () => {
+            return LyricService.getAllLyrics(db)
+                .then(actual => {
+                    expect(actual).to.eql([])
+                })
+        })
+        it(`insertLyric() inserts a new lyric and resolves the new lyric with an 'id'`, () => {
+            const newLyric = {
+                title: "Hello",
+                genre: "Rap",
+                mood: "Happy",
+                artist: "Foo",
+                lyrics: "Leo sociosqu sagittis nascetur netus congue? Dapibus cubilia praesent nam magnis ante felis Leo sociosqu sagittis nascetur netus congue? Dapibus cubilia praesent nam magnis ante felis Leo sociosqu sagittis nascetur netus congue? Dapibus cubilia praesent nam magnis ante felis"
+            }
+            return LyricService.insertLyric(db, newLyric)
+                .then(actual => {
+                    expect(actual).to.eql({
+                        id: 1,
+                        title: "Hello",
+                        genre: "Rap",
+                        mood: "Happy",
+                        artist: "Foo",
+                        lyrics: "Leo sociosqu sagittis nascetur netus congue? Dapibus cubilia praesent nam magnis ante felis Leo sociosqu sagittis nascetur netus congue? Dapibus cubilia praesent nam magnis ante felis Leo sociosqu sagittis nascetur netus congue? Dapibus cubilia praesent nam magnis ante felis"
+                    })
+                })
         })
     })
 })
