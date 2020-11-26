82a90d29d3dc929070dbf3168523120e4ca9d6af
JaKel Jenkins <jakelj24@gmail.com>
2020-11-26 15:45:54 -0500

    updated

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api",
+    // API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api",
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL|| 'postgresql://jakel:hello@localhost/lyric_dbase',
+    DATABASE_URL: process.env.DATABASE_URL|| 'postgresql://jakel:hello@localhost/lyric_dbase',
     TOKEN_KEY: 'ghostwriterz-client-auth-token',
     JWT_SECRET: process.env.JWT_SECRET ||'change-this-secret',
   }
