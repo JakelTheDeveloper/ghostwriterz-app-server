c933a1a3aa84a86cae5cffd5f93814854327543f
JaKel Jenkins <jakelj24@gmail.com>
2020-12-11 01:09:39 -0500

    updated

--- src/app.js
+++ src/app.js
@@ -28,22 +28,6 @@ app.use('/api/users', usersRouter)
 app.use('/api/lyrics', lyricRouter)
 
 
-//Set Up validate Token
-
-// app.use(function validateBearerToken(req, res, next) {
-//   const apiToken = process.env.API_TOKEN
-//   const authToken = req.get('Authorization')
-
-//   if (!authToken || authToken.split(' ')[1] !== apiToken) {
-//     logger.error(`Unauthorized request to path: ${req.path}`)
-//     return res.status(401).json({ error: 'Unauthorized request' })
-//   }
- 
-//   next()
-// })
-
-
-
 app.use(function errorHandler(error, req, res, next) {
   let response
   if (NODE_ENV === 'production') {
